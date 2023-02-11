248da3b21b1e1618b70e1e0c226c217133a019b9
Matvey <mathway.home@gmail.com>
2023-02-11 12:08:27 +0300

    updated path to preserved

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 prefix=$HOME
-
-for f in $(cat ${0%/*}/preserved); do
-	if ! ln -rs "$f" "$prefix/$f" 2>/dev/null && [ ! -L "$prefix"/"$f" ]; then
+SH=${0%/*}
+for f in $(cat $SH/preserved); do
+	if ! ln -rs $SH/"$f" "$prefix/$f" 2>/dev/null && [ ! -L "$prefix"/"$f" ]; then
 		printf "[WARNING] Ignoring '$f'\n"
 	fi
 done
